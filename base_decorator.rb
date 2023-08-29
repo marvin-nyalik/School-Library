1b1ac77ab4a0f297fcbe794c9df65b51c26b29d6
MARVIN <o.silverygold@gmail.com>
2023-08-29 13:05:24 +0300

    Add base decorator

---
+++ base_decorator.rb
@@ -0,0 +1,12 @@
+class BaseDecorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
