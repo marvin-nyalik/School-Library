67b5eb573e1666e9d412b7c39426c7e506674107
MARVIN <o.silverygold@gmail.com>
2023-08-31 16:50:38 +0300

    Alter app for UI

--- person.rb
+++ person.rb
@@ -5,7 +5,7 @@ class Person < Nameable
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = Random.rand(1..10_000)
+    @id = Random.rand(1..1_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
