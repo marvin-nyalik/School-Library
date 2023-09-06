17bee29c47e6d637b8a2fe28968708bf44ab71b2
MARVIN <o.silverygold@gmail.com>
2023-09-06 14:05:54 +0300

    Fix rubocop errors

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,14 +1,14 @@
 require_relative 'spec_helper'
-​
+
 describe Teacher do
   before(:each) do
     @teacher = Teacher.new(34, 'Money Market', 'Mackenzie')
   end
-​
+
   it 'is initialized correctly' do
     expect(@teacher.name).to eq('Mackenzie')
   end
-​
+
   it 'can always use services' do
     expect(@teacher.can_use_services?).not_to be_falsey
   end
--- spec/upcase_deco_spec.rb
+++ spec/upcase_deco_spec.rb
@@ -1,15 +1,15 @@
 require_relative 'spec_helper'
-​
+
 describe UpcaseDecorator do
   it 'delegates the call to correct_name method of the wrapped Nameable object' do
     nameable = Nameable.new
     allow(nameable).to receive(:correct_name).and_return('Parthenogenesis')
-​
+
     base_dec = BaseDecorator.new(nameable)
     upcase_dec = UpcaseDecorator.new(base_dec)
-​
+
     result = upcase_dec.correct_name
-​
+
     expect(result).to eq('PARTHENOGENESIS')
   end
 end
